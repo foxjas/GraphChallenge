0aa837cc38a1e3db5ec83d25bafb0c312d2eac61
foxjas <foxjas09@gmail.com>
2017-05-20 14:38:50 -0400

    modified ktruss python script

--- SubgraphIsomorphism/ktruss/code/python/ktruss.py
+++ SubgraphIsomorphism/ktruss/code/python/ktruss.py
@@ -64,8 +64,8 @@ def ktruss (inc_mat_file,k):
     tmp=np.transpose(E)*E
     sizeX,sizeY=np.shape(tmp)
     
-    print "Time to Read Data:  " + str(readTime-startTime) + "s"
-    print "Computing k-truss"
+    # print("Time to Read Data:  " + str(readTime-startTime) + "s")
+    # print("Computing k-truss")
     tmp.setdiag(np.zeros(sizeX),k=0)
     #set_diag_val(tmp,0)
     tmp.eliminate_zeros()
@@ -75,8 +75,8 @@ def ktruss (inc_mat_file,k):
     xc= (s >=k-2).astype(int)
     
     while xc.sum() != np.unique(sp.sparse.find(E)[0]).shape:
-	x=sp.sparse.find(xc==0)[0]
-	#x=np.where(xc==0)[0]
+        x=sp.sparse.find(xc==0)[0]
+        #x=np.where(xc==0)[0]
         set_zero_rows(E, x)
         E.eliminate_zeros()
         tmp=np.transpose(E)*E
@@ -86,7 +86,7 @@ def ktruss (inc_mat_file,k):
         xc= (s >=k-2).astype(int)
 
     ktrussTime=time.clock()
-    print "Time to Compute k=truss:  " + str(ktrussTime-startTime) + "s"
+    print("k={}: {}".format(k, ktrussTime-startTime))
     return E
 
 
--- SubgraphIsomorphism/ktruss/code/python/runKtrussBenchmark.py
+++ SubgraphIsomorphism/ktruss/code/python/runKtrussBenchmark.py
@@ -1,20 +1,29 @@
 import numpy as np
 import scipy as sp
+import sys
 from ktruss import ktruss
 
 #Use the pandas package if available
 #import pandas as pd
 
-inc_mtx_file = '../../../data/ktruss_example.tsv';
+args = sys.argv[1:]
+if not len(args) == 2:
+	print("Usage: [input] [k]")
+	sys.exit()
 
-E_expected=np.array([ (1, 1, 0 ,0 ,0), (0, 1, 1 ,0 ,0), (1, 0,0,1,0),(0,0,1,1,0),(1,0,1,0,0),(0,0,0,0,0) ])
+# inc_file = '../../../data/ktruss_example.tsv';
+inc_file = args[0]
+k = int(args[1])
 
-E=ktruss(inc_mtx_file,3)
+# E_expected=np.array([ (1, 1, 0 ,0 ,0), (0, 1, 1 ,0 ,0), (1, 0,0,1,0),(0,0,1,1,0),(1,0,1,0,0),(0,0,0,0,0) ])
 
-if sum(sum(E.toarray()-E_expected)):
-    print "unable to verify"
-else:
-    print E
+E=ktruss(inc_file,k)
+# print(E.toarray())
+
+# if sum(sum(E.toarray()-E_expected)):
+#     print("unable to verify")
+# else:
+#     print(E.toarray()) # verbose option?
 
 ###################################################
 # Graph Challenge benchmark
