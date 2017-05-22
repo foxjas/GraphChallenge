d464237633815e36d5efb16ed11cb86c878ef8f5
foxjas <foxjas09@gmail.com>
2017-05-22 19:24:09 -0400

    updated scripts

--- scripts/ktruss_matlab.sh
+++ scripts/ktruss_matlab.sh
@@ -2,19 +2,21 @@
 
 OUT=$1
 INPUT_DIR=/data/GraphChallenge-17
-APP=$HOME/GraphChallenge/SubgraphIsomorphism/ktruss/code/python/runKtrussBenchmark.py
-PYTHON=$HOME/anaconda/bin/python3
+APP_DIR=$HOME/GraphChallenge/SubgraphIsomorphism/ktruss/code/matlab
+APP=${APP_DIR}/runKtrussBenchmark.m
+EXE=octave
 # snap_graphs=(amazon0302 cit-Patents)
 # scale_graphs=(graph500-scale18-ef16 graph500-scale19-ef16 graph500-scale20-ef16)
 graphs=(amazon0302 cit-Patents graph500-scale18-ef16)
-# K=(3 6 12 24 48)
-K=(6 12 24 48)
+K=(3 6 12 24 48)
+
+cd ${APP_DIR}
 for k in "${K[@]}"
 do
 	for graph in "${graphs[@]}"
 	do
 		input="${INPUT_DIR}/${graph}_inc.tsv"
 		echo "graph=${graph},k=$k" >> ${OUT}
-		${PYTHON} ${APP} ${input} $k >> ${OUT}
+		${EXE} ${APP} ${input} $k >> ${OUT}
 	done
 done
