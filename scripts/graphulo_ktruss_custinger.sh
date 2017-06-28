e625ab4d141ef3cc6caa2d23ba6e7913c6eebfd6
foxjas <foxjas09@gmail.com>
2017-06-27 21:04:52 -0400

    script for running graphulo graphs on custinger

---
+++ scripts/graphulo_ktruss_custinger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OUT=$1
+MODE=$2
+
+INPUT_DIR=$DATA
+APP=$HOME/ktruss2/build/ktruss
+
+graphs=(graphulo-scale10 graphulo-scale11 graphulo-scale12 graphulo-scale13 graphulo-scale14 graphulo-scale15 graphulo-scale16)
+
+
+if [ "$MODE" == 4 ]
+then
+	for graph in "${graphs[@]}"
+	do
+		input="${INPUT_DIR}/${graph}.mmio"
+		echo "graph=${graph},k=3" >> ${OUT}
+		${APP} ${input} 4 3 >> ${OUT}
+		echo >> ${OUT}
+	done
+else
+	for graph in "${graphs[@]}"
+	do
+		input="${INPUT_DIR}/${graph}.mmio"
+		echo "graph=${graph},k=3" >> ${OUT}
+		${APP} ${input} 8 3 >> ${OUT}
+		echo >> ${OUT}
+	done
+fi
